59c80103fd144381bda051ad8248bcd3b5fc7eb8
unknown <anabubblegum765@gmail.com>
2025-01-29 14:19:38 -0300

    Second Commit

--- src/main/java/Controller/InstitutionController.java
+++ src/main/java/Controller/InstitutionController.java
@@ -1,22 +1,61 @@
 package Controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import Models.Institution;
+import Repository.InstitutionRepo;
 import ch.qos.logback.core.model.Model;
 
 @Controller
 @RequestMapping("/prot")
 public class InstitutionController {
 	@Autowired
-
+	private InstitutionRepo institutionrepo;
 	@PostMapping("/createInst")
-	public void CreateInstitution(@ModelAttribute Institution institution, Model model) {
-
+	public void CreateInstitution(@ModelAttribute  Model model, 
+		@RequestParam("id")Optional<Long>id,
+		@RequestParam("nameInstitution") String nameInstitution,
+		@RequestParam("cnpjInstitution") String cnpjInstitution,
+		@RequestParam("unitInstution")  String unitInstution
+		){	
+		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/Models/Institution.java
+++ src/main/java/Models/Institution.java
@@ -5,17 +5,48 @@ import org.springframework.data.annotation.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity // -> Definir classe
 @NoArgsConstructor // -> Construtor
 @Data // -> Para get and setters
+@Getter
+@Setter
 public class Institution {
 	@Id
 	@GeneratedValue
 	private Long idInstitution;
-	 String nameInstitution;
-	 String cnpjInstitution;
-	 String unitInstution;  
+	private String nameInstitution;
+	 private String cnpjInstitution;
+	private String unitInstution;
+	public Long getIdInstitution() {
+		return idInstitution;
+	}
+	public void setIdInstitution(Long idInstitution) {
+		this.idInstitution = idInstitution;
+	}
+	public String getNameInstitution() {
+		return nameInstitution;
+	}
+	public void setNameInstitution(String nameInstitution) {
+		this.nameInstitution = nameInstitution;
+	}
+	public String getCnpjInstitution() {
+		return cnpjInstitution;
+	}
+	public void setCnpjInstitution(String cnpjInstitution) {
+		this.cnpjInstitution = cnpjInstitution;
+	}
+	public String getUnitInstution() {
+		return unitInstution;
+	}
+	public void setUnitInstution(String unitInstution) {
+		this.unitInstution = unitInstution;
+	}  
+	 
+	 
+	 
 	 
 }
